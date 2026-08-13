0c1899ca231a6a35a7e240b883c8e03cf5404171
LinnKarlsen <linnhelen@gmail.com>
2026-08-13 16:24:54 -0400

    Clock divisor works in simulation.

---
+++ Exp1/Exp1.srcs/sources_1/new/divisor_de_clk.vhd
@@ -0,0 +1,25 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity divisor_de_clk is
+    Port ( clk_in : in STD_LOGIC;
+           clk_in_out_ratio : in INTEGER;
+           clk_out : out STD_LOGIC);
+end divisor_de_clk;
+
+architecture Behavioral of divisor_de_clk is
+    signal clk_out_internal: STD_LOGIC:= '0';
+begin
+    process(clk_in)
+        variable counter: INTEGER:= 0;
+    begin
+        if rising_edge(clk_in) then
+            counter := counter + 1;
+        end if;
+        if counter = clk_in_out_ratio then
+            clk_out_internal <= not clk_out_internal;
+            counter := 0;
+        end if;
+    end process;
+    clk_out <= clk_out_internal;
+end Behavioral;
